d2112f86e1b411867fd121f4087cd66da5d27974
Ronald Rink <dfch@users.noreply.github.com>
2026-05-12 11:53:07 +0200

    Iso25010
    * extend characteristics
    * add spelling

--- src/biz/dfch/incoseiso25010refiner/iso25010.py
+++ src/biz/dfch/incoseiso25010refiner/iso25010.py
@@ -16,9 +16,33 @@
 """ISO25010 characteristics."""
 
 from enum import StrEnum
+from dataclasses import dataclass
 
 
 class Iso25010(StrEnum):
+    """ISO25010 main characteristics."""
+
+    FUNCTIONALITY = "functional suitability"
+    PERFORMANCE = "performance	efficiency"
+    COMPATIBILITY = "compatibility"
+    INTERACTION = "interaction capability"
+    RELIABILITY = "reliability"
+    SECURITY = "security"
+    MAINTAINABILITY = "maintainability"
+    FLEXIBILITY = "flexibility"
+    SAFETY = "safety"
+
+
+class Iso25010Functionality(StrEnum):
+    """ISO25010 characteristics and description of 'functional suitability'."""
+
+    FUNCTIONAL_COMPLETENESS = "capability of a product to provide a set of functions that covers all the specified tasks and intended users’ objectives"
+    FUNCTIONAL_CORRECTNESS = "capability of a product to provide accurate results when used by intended users"
+    FUNCTIONAL_APPROPRIATENESS = "capability of a product to provide functions that facilitate the accomplishment of specified tasks and objectives"
+
+
+@dataclass(frozen=True)
+class Iso25010Characteristics:
     """ISO25010 characteristics."""
 
-    FUNCTIONAL = "Functional suitability"
+    FUNCTIONALITY = Iso25010Functionality  # pylint: disable=C0103
